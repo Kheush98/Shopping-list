2a6d814aefa4f0f982ff0fcd8cb5f028881d94f1
Cheikh GAYE <gayec270198@gmail.com>
2023-11-12 12:02:35 +0000

    addind filter items

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const itemForm = document.getElementById('item-form')
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
 const clearBtn = document.getElementById('clear')
+const itemFilter = document.getElementById('filter')
 
 
 function addItem (e) {
@@ -19,8 +20,9 @@ function addItem (e) {
     const button = createButton('remove-item btn-link text-red')
    
     li.appendChild(button)
+    // Add item
     itemList.append(li)
-
+    checkUI();
     itemInput.value = ''
 }
 
@@ -38,8 +40,9 @@ function createButton (className) {
 
 function deleteItem (e) {
     const item = e.target
-    if (item.tagName == 'I') {
-        item.parentElement.parentElement.remove();
+    if (item.tagName == 'I' && confirm("Are you sure ?")) {
+        item.parentElement.parentElement.remove();           
+        checkUI()
     }
 }
 
@@ -47,8 +50,40 @@ function clearItem () {
     while (itemList.firstChild) {
         itemList.removeChild(itemList.firstChild)
     }
+    checkUI()
+}
+
+function checkUI () {
+    const items = itemList.querySelectorAll('li');
+    if (items.length === 0) {
+        itemFilter.style.display = 'none'
+        clearBtn.style.display = 'none'
+    } else {
+        itemFilter.style.display = 'block'
+        clearBtn.style.display = 'block'
+    }
+
+
+}
+
+function filterItem (e) {
+    const items = itemList.querySelectorAll('li')
+    const filterValue = e.target.value.toLowerCase() 
+
+    items.forEach(item => {
+        const itemName = item.firstChild.textContent.toLowerCase()
+        
+        if (!itemName.includes(filterValue)) {
+            item.style.display = 'none'
+        } else {
+            item.style.display = 'flex'
+        }
+    })
 }
 // Event listeners
 itemForm.addEventListener('submit', addItem)
 itemList.addEventListener('click', deleteItem)
 clearBtn.addEventListener('click', clearItem)
+itemFilter.addEventListener('input', filterItem)
+
+checkUI()
